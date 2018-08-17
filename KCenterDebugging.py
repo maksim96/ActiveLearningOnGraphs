max flow from graph_tool and own min cut works now as expected. Quite sensible to scaling of X and sigma. np.exp can lead to wrong zeros.

KCenterDebugging.py
@@ -1,11 +1,5 @@
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-import graph_tool
 # from sympy.core.basic import preorder_traversal
 
 import scipy.spatial
@@ -15,18 +9,7 @@ import graph_tool as gt
 from graph_tool import flow
 from graph_tool.all import graph_draw
 
-class VisitorExample(gt.search.BFSVisitor):
-
-    reachable_vertices = []
-
-    first_time = True #we don't want to add the root
-
-    def discover_vertex(self, u):
-        if self.first_time:
-            self.first_time = False
-        else:
-            self.reachable_vertices.append(g2.vertex_index[u])
-
+import MinCut
 
 def compute_k_center(dists, k):
     center = [np.random.randint(0, dists.shape[0])]
@@ -62,7 +45,6 @@ def mincut_strategy(W, L):
     print(mid - start)
 
     cut_value, (zero_class, positive_class) = nx.minimum_cut(g, 's', 't', capacity='weight')
-    print(zero_class)
     print("--> " + str(cut_value))
     cut_time = time.time()
     print(cut_time - mid)
@@ -76,6 +58,7 @@ def mincut_strategy(W, L):
             prediction[i] = 1
     print(time.time() - cut_time)
     return prediction
+
 
 
 def faster_min_cut_strategy(W, L):
@@ -105,24 +88,14 @@ def faster_min_cut_strategy(W, L):
     #almost_full = np.greater(res.a, weight.a - eps)
     #res.a[almost_full] = weight.a[almost_full]
 
-    pos = g2.new_vertex_property("vector<double>")
-    pos.set_2d_array(np.transpose(np.vstack((X, np.array([-3,0]), np.array([8,0])))))
+    #drawing
+    #pos = g2.new_vertex_property("vector<double>")
+    #pos.set_2d_array(np.transpose(np.vstack((X, np.array([-3,0]), np.array([8,0])))))
+    #graph_draw(g2, pos=pos, edge_pen_width=gt.draw.prop_to_size(res), output="output.pdf")
 
-    graph_draw(g2, pos=pos, edge_pen_width=gt.draw.prop_to_size(res), output="output.pdf")
+    #part = flow.min_st_cut(g2, s, weight, res) #part[i] == true <=> vertex i is in the partition of the source s
 
-    part = flow.min_st_cut(g2, s, weight, res) #part[i] == true <=> vertex i is in the partition of the source s
-
-    nonzero_res = g2.new_edge_property("bool")
-    nonzero_res.a = res.a > 0
-
-    g2.set_edge_filter(nonzero_res)
-
-
-    visitor = VisitorExample()
-
-    gt.search.bfs_search(g2, s, visitor)
-
-    print(visitor.reachable_vertices)
+    part = MinCut.own_min_cut(g2, s, weight, res)
 
     max_flow = sum((weight[e] - res[e]) for e in g2.vertex(t).in_edges())
     print("-->:" + str(max_flow))
@@ -130,15 +103,8 @@ def faster_min_cut_strategy(W, L):
     cut_time = time.time()
     print(cut_time - mid)
 
-    print(part.a)
-
     prediction = np.ones(W.shape[0])
-    '''for i in range(W.shape[0]):
-        if part[i]:
-            prediction[i] = 0
-        else:
-            prediction[i] = 1'''
-    prediction[visitor.reachable_vertices] = 0
+    prediction[part] = 0
     print(time.time() - cut_time)
     return prediction
 
@@ -224,7 +190,6 @@ g2.add_edge_list(edges, eprops=eprops)
 
 
 positions = compute_k_center(dists, 20)
-print(X[positions, :])
 plt.scatter(X[positions,0], X[positions,1], alpha=0.5, s=20, c='g')
 plt.show()
 L = np.zeros((X.shape[0], 2))

KCenterTest.py
@@ -4,14 +4,16 @@ import graph_tool
 #from sympy.core.basic import preorder_traversal
 
 import LocalAndGlobalTest
+import MinCut
 import scipy.spatial
 import networkx as nx
 import time
 import graph_tool as gt
 from graph_tool import flow
 
-def compute_k_center(dists, k):
-    center = [np.random.randint(0,dists.shape[0])]
+
+def compute_k_center(dists, k, first_vertex):
+    center = [first_vertex]
     #dists = dists[~np.eye(dists.shape[0],dtype=bool)].reshape(dists.shape[0],-1) #remove zero diagonal
     #np.fill_diagonal(dists, upper_bound)
     for i in range(1,k):
@@ -21,12 +23,10 @@ def compute_k_center(dists, k):
             distsToCenters = dists[:,center]
             tempidx = np.argmax(np.min(distsToCenters, axis=1))
         else:
-            tempidx = np.argmax(dists[center[0],mask])
+            tempidx = np.argmax(dists[center[0]])
         center.append(tempidx) #+ sum(j <= tempidx + 1 for j in center))
 
     return center
-
-
 
 def mincut_strategy(W, L):
     start = time.time()
@@ -58,6 +58,7 @@ def mincut_strategy(W, L):
     print(time.time() - cut_time)
     return prediction
 
+
 def faster_min_cut_strategy(W, L):
     start = time.time()
 
@@ -74,51 +75,50 @@ def faster_min_cut_strategy(W, L):
 
     mid = time.time()
     print(mid - start)
-
     res = flow.push_relabel_max_flow(g2, s, t, weight)
-    part = flow.min_st_cut(g2, s, weight, res)
-
     max_flow = sum((weight[e] - res[e]) for e in g2.vertex(t).in_edges())
-    print("-->:" +str(max_flow))
+    print("-->:" + str(max_flow))
+
+    part = MinCut.own_min_cut(g2, s, weight, res)
+
+
 
     cut_time = time.time()
     print(cut_time - mid)
 
-    prediction = np.zeros(W.shape[0])
-    for i in range(W.shape[0]):
-        if part[i]:
-            prediction[i] = 0
-        else:
-            prediction[i] = 1
+    prediction = np.ones(W.shape[0])
+    prediction[part] = 0
     print(time.time() - cut_time)
     return prediction
 
-dataset = 4
+dataset = 2
 
 X = np.genfromtxt('res/benchmark/SSL,set='+str(dataset)+',X.tab')
+#standardize
+X = (X - np.min(X, axis=0)) / (np.max(X, axis=0) - np.min(X, axis=0))
 y = 0.5*(np.genfromtxt('res/benchmark/SSL,set='+str(dataset)+',y.tab')+1) #1 --> 1, -1 --> 0
 
 dists = scipy.spatial.distance.cdist(X, X)
 
-average_knn_dist = np.average(np.sort(X)[:,:5])
+#average_knn_dist = np.average(np.sort(X)[:,:5])
 
-sigma = np.min(dists[np.nonzero(dists)])
+sigma = 1#np.min(dists[np.nonzero(dists)])
 W = np.exp(-(dists) ** 2 / (2 * sigma ** 2))
 np.fill_diagonal(W, 0)
 #W2 = np.copy(W) less edges is slower strangely
 #W2[W2 <= 0.1] = 0
 
 g = nx.from_numpy_matrix(W)
-upper_bound = 200 * np.sum(W)
+upper_bound = 2*np.sum(W)
 g.add_node('s')
 g.add_node('t')
 
 np.nonzero(W.flatten())
 weights = W.flatten()
-weights[weights != 0]
 a, b = W.nonzero()
 edges = np.transpose(np.vstack((a, b, weights[weights != 0])))
 
+np.random.seed(0)
 
 g2 = gt.Graph()
 
@@ -127,29 +127,34 @@ g2.add_vertex(W.shape[0])
 # add source, sink
 s = g2.vertex_index[g2.add_vertex()]
 t = g2.vertex_index[g2.add_vertex()]
-weight = g2.new_edge_property("double")
+weight = g2.new_edge_property("long double")
 eprops = [weight]
 g2.add_edge_list(edges, eprops=eprops)
 
+first_vertex = np.random.randint(0, dists.shape[0])
 
-for i in range(11):
-    if i > 0:
-        positions = [int(idx) for idx in list(np.genfromtxt('res/benchmark/SSL,set='+str(dataset)+',splits,labeled=10.tab')[i-1] - 1)]
-    else:
-        positions = compute_k_center(dists, 100)
+for i in range(1,21):
+    #if i > 0:
+     #   positions = [int(idx) for idx in list(np.genfromtxt('res/benchmark/SSL,set='+str(dataset)+',splits,labeled=100.tab')[i-1] - 1)]
+    #else:
+
+
+
+    positions = compute_k_center(dists, i*20,first_vertex)
     L = np.zeros((X.shape[0], 2))
     L[positions, 0] = 1 - y[positions]
     L[positions, 1] = y[positions]
     L = L.astype(int)
 
     predictionIterative = LocalAndGlobalTest.predict(X,L,sigma,0.5,200)
-    prediction_min_cut = mincut_strategy(W, L)
+    #prediction_min_cut = mincut_strategy(W, L)
     prediction_faster_min_cut = faster_min_cut_strategy(W, L)
-    prediction_random = np.random.randint(2, size=prediction_min_cut.shape)
+    prediction_random = np.random.randint(2, size=prediction_faster_min_cut.shape)
 
 
     print(np.dot(predictionIterative-y,predictionIterative-y))
-    print(np.dot(prediction_min_cut - y, prediction_min_cut - y))
+    #print(np.dot(prediction_min_cut - y, prediction_min_cut - y))
     print(np.dot(prediction_faster_min_cut - y, prediction_faster_min_cut - y))
     print(np.dot(prediction_random-y,prediction_random-y))
     print("==================================================")
+    print(str((i+1)*20)+".")